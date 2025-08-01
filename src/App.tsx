b6358f45f03d30cdab947c656f62c7bd3497a1a9
Elise Badun <elliebadun@gmail.com>
2025-08-01 11:33:08 -0700

    Initial commit, styling and outline

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,133 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
+    <>
+      <div style={{ overflow: "hidden" }}>
+        <div className="scrolling-text">🌟 WARNING - WORK IN PROGRESS 🌟</div>
+      </div>
+      <BasicLayout>
+        <Section>
+          <h2>Welcome to my site!</h2>
+          <p>hello world! welcome to my home 🫧😎👽</p>
+          <img
+            src="https://media.tenor.com/vt3W-PX-GUkAAAAC/welcome-to-my-home-gary-oldman.gif"
+            alt="Dracula also welcomes you"
+          />
+        </Section>
+      </BasicLayout>
+    </>
+  );
+}
+
+function Section({ children }: { children: React.ReactNode }) {
+  return <div className="section">{children}</div>;
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>~*~ ellie's special spot~*~</h1>
+      <h2 style={{ textAlign: "center" }}>enter, if u dare</h2>
+
+      <nav className="navbar">
+        <ul>
+          <li>
+            <a href="/">home</a>
+          </li>{" "}
+          <li>
+            <a href="/about">about</a>
+            <ul>
+              <li>
+                <a href="/fun-stuff/about-me">about me</a>
+              </li>
+              <li>
+                <a href="/fun-stuff/about-the-site">about the site</a>
+              </li>
+              <li>
+                <a href="/fun-stuff/guestbook">guestbook</a>
+              </li>
+            </ul>
+          </li>{" "}
+          <li>
+            <a href="/my-stuff">my stuff</a>
+            <ul>
+              <li>
+                <a href="/fun-stuff/spotify-playlists">spotify playlists</a>
+              </li>
+              <li>
+                <a href="/fun-stuff/coding-projects">coding projects</a>
+              </li>
+              <li>
+                <a href="/fun-stuff/media-i-love">media I love</a>
+              </li>
+              <li>
+                <a href="/fun-stuff/kombucha-blog">kombucha blog</a>
+              </li>
+            </ul>
+          </li>{" "}
+          <li>
+            <a href="/fun-stuff">fun stuff</a>
+            <ul>
+              <li>
+                <a href="/fun-stuff/games">games</a>
+              </li>
+              <li>
+                <a href="/fun-stuff/blinkies">blinkies</a>
+              </li>
+              <li>
+                <a href="/fun-stuff/polls">polls</a>
+              </li>
+              <li>
+                <a href="/fun-stuff/personality-quizzes">personality quizzes</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+function Sidebar() {
+  return <div className="sidebar">Sidebar</div>;
+}
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>
+        <span className="blink">Under Construction 🚧</span>
+      </p>
+      <p>&copy; 1999-2025 Ellie’s Totally Awesome Blog</p>
+      <p>
+        <img
+          src="https://www.animatedimages.org/data/media/171/animated-under-construction-image-0040.gif"
+          alt="Under Construction"
+        />
+        <img
+          src="https://www.animatedimages.org/data/media/113/animated-email-image-0008.gif"
+          alt="Email Me!"
+        />
+      </p>
+    </div>
+  );
+}
+
+function MainContent({ children }: { children: React.ReactNode }) {
+  return <div className="main-content">{children}</div>;
+}
+
+function BasicLayout({ children }: { children: React.ReactNode }) {
+  return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="layout">
+        <Header />
+        <Sidebar />
+        <MainContent>{children}</MainContent>
+        <Footer />
+      </div>
     </div>
   );
 }
