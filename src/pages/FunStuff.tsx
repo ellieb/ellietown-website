a10f3600b016802be394c3c2522027c8af034b17
Elise Badun <elliebadun@gmail.com>
2025-08-01 17:10:09 -0700

    Add redirects file, cleanup a little

--- src/pages/FunStuff.tsx
+++ src/pages/FunStuff.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import BasicLayout from "../components/BasicLayout";
-import Section from "../components/Section";
+// import Section from "../components/Section";
 
 function FunStuff() {
   return (
     <BasicLayout>
       <p>Look at all this fun stuff here..</p>
-      <Section title={"Song game"}>
+      {/* <Section title={"Song game"}>
         Guess when the song was released? Inspired by Hitster.
-        {/* Can choose specific playlist perhaps? But defaults to 
+
+      </Section> */}
+      {/* Can choose specific playlist perhaps? But defaults to 
         some best 1000 songs of the last 100 years maybe */}
-      </Section>
 
       {/* <div>games</div>
       <div>blinkies</div>
